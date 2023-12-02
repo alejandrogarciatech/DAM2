ef85ee849d57921252a5bc3c345c1f32da1f9753
alesgsound <alejandrogarcia.tech@gmail.com>
2023-12-02 01:49:49 +0100

    Clase 7, minuto 01:07:00

---
+++ PMDM/ListasJSON/app/src/main/java/com/example/listasjson/adapter/UsuariosAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.listasjson.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class UsuariosAdapter: RecyclerView.Adapter<UsuariosAdapter.MyHolder>() {
+    class MyHolder (item: view): RecyclerView.ViewHolder(item){}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
