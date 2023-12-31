b959a629d23afe6356159699a3e5dd5405bff7fc
alesgsound <alejandrogarcia.tech@gmail.com>
2023-12-31 03:24:38 +0100

    Actualización 31/12/23

--- PMDM/Feedback3_audio/app/src/main/java/com/example/feedback3_audio/MainActivity.kt
+++ PMDM/Feedback3_audio/app/src/main/java/com/example/feedback3_audio/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.feedback3_audio
 
-import android.annotation.SuppressLint
 import android.icu.text.SimpleDateFormat
-import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.os.Environment
@@ -16,60 +14,60 @@ import java.util.Locale
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+    //Declaramos la variable grabación como nula porque aún no existe
     private var grabacion: MediaRecorder? = null
-    private var outputFile: String? = null
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Declaramos las variables correspondientes a los botones y textview
         val grabar: ImageButton = findViewById(R.id.botonGrabar)
         val parar: ImageButton = findViewById(R.id.botonParar)
-        val reproducir: ImageButton = findViewById(R.id.botonReproducir)
         val ultimoAudio: TextView = findViewById(R.id.audioTextView)
 
+        //Declaramos la variable timeStamp para darle un nombre a cada audio grabado, con la fecha y la hora en la que han sido creados
         val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+
+        //Así evitamos que el mismo archivo se sobreescriba una y otra vez
         val nombreAudio: String = "audio_$timeStamp.3gp"
 
+        //Construimos la ruta completa del archivo de audio concatenando el directorio de almacenamiento externo, un separador ( / ) y el nombre del archivo
         val outputFile: String = Environment.getExternalStorageDirectory().absolutePath + File.separator + nombreAudio
-        val rutaUltimoAudio: String = "audio_$timeStamp.3gp"
 
+        //Creamos una instancia de MediaRecorder,que se utiliza para grabar audio y vídeo
         grabacion = MediaRecorder()
+
+        //Establecemos la fuente de audio, el formato de salida, el codificador y la ruta  en la que se guardará el archivo
         grabacion?.setAudioSource(MediaRecorder.AudioSource.MIC)
         grabacion?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
         grabacion?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
         grabacion?.setOutputFile(outputFile)
 
+        //Establecemos un listener para detectar la pulsación del botón grabar
         grabar.setOnClickListener {
-            try {
+            try {//Se prepara el dispositivo y se inicia la grabación
                 grabacion?.prepare()
                 grabacion?.start()
+                //Cambiamos la imagen del botón para indicar al usuario que la grabación está en curso
                 grabar.setImageResource(R.drawable.grabando)
             } catch (e: IllegalStateException) {
                 e.printStackTrace()
             } catch (e: IOException) {
                 e.printStackTrace()
-            }
+            }//Manejamos las excepciones para ver los posibles fallos por consola
         }
 
+        //Establecemos un listener para detectar la pulsación del botón parar
         parar.setOnClickListener {
+            //Dejamos la imagen original del botón grabar para indicar que ya no se está grabando
             grabar.setImageResource(R.drawable.grabar)
+            //Se para la grabación, se liberan recursos y se elimina la referencia al objeto MediaRecorder
             grabacion?.stop()
             grabacion?.release()
             grabacion = null
+            //Cuando la grabación ha finalizado, mostramos al usuario el nombre del audio recién creado
             ultimoAudio.setText(nombreAudio)
-        }
-
-        reproducir.setOnClickListener {
-            val mediaPlayer = MediaPlayer()
-            try {
-                mediaPlayer.setDataSource(rutaUltimoAudio)
-                mediaPlayer.prepare()
-                mediaPlayer.start()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
         }
     }
 }
--- PMDM/Feedback3_video/app/src/main/java/com/example/feedback3_video/MainActivity.kt
+++ PMDM/Feedback3_video/app/src/main/java/com/example/feedback3_video/MainActivity.kt
@@ -11,14 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Declaramos la variable que hace referencia al VideoView de la pantalla
         val videoView: VideoView = findViewById(R.id.video)
+
+        //Creamos un MediaController que proporcione controles de reproducción, pausa y avance
         val mediaController = MediaController(this)
+        //Se establece la vista anclada, para que se superponga al VideoView
         mediaController.setAnchorView(videoView)
 
+        //Creamos una URI que apunta a la ubicación del recurso de vídeo, en la carpeta raw que hemos creado
         val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.resurrection)
+        //Establecemos la ruta y los controladores en el VideoView
         videoView.setMediaController(mediaController)
         videoView.setVideoURI(videoUri)
+        //requestFocus prepara el componente para recibir interacciones
         videoView.requestFocus()
+        //Se inicia la reproducción del vídeo
         videoView.start()
     }
 }
